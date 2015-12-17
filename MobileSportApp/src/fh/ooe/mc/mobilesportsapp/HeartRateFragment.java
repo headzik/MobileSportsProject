c37154383b356874dd3d7f09285c4eb15d183111
KathiSick <katharina.sick@hotmail.com>
2015-12-17 14:01:41 +0100

    heartrateservice (not working yet)

--- MobileSportApp/src/fh/ooe/mc/mobilesportsapp/HeartRateFragment.java
+++ MobileSportApp/src/fh/ooe/mc/mobilesportsapp/HeartRateFragment.java
@@ -2,6 +2,7 @@ package fh.ooe.mc.mobilesportsapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,6 +20,20 @@ public class HeartRateFragment extends Fragment{
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		View rootView = inflater.inflate(R.layout.fragment_heartrate, container, false);
+		HardwareConnectorServiceConnection con = new HardwareConnectorServiceConnection(getActivity(), new HardwareConnectorServiceConnection.Listener() {
+			
+			@Override
+			public void onHardwareConnectorServiceDisconnected() {
+				Log.i("connector", "disconnected");
+			}
+			
+			@Override
+			public void onHardwareConnectorServiceConnected(HardwareConnectorService hardwareConnectorService) {
+				Log.i("connector", "connected");
+				
+			}
+		});
+		
 		return rootView;
 	}
 	
