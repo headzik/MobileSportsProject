50aad95ade8bd5094afc12fd777ab0c7303ea582
headzik <lukaszstrzelecki9@gmail.com>
2016-01-07 15:22:24 +0100

    Dividing signing up and loggin in

--- MobileSportApp/src/fh/ooe/mc/mobilesportsapp/LoginSignupActivity.java
+++ MobileSportApp/src/fh/ooe/mc/mobilesportsapp/LoginSignupActivity.java
@@ -3,9 +3,9 @@ package fh.ooe.mc.mobilesportsapp;
 import com.parse.LogInCallback;
 import com.parse.ParseException;
 import com.parse.ParseUser;
-import com.parse.SignUpCallback;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -26,7 +26,7 @@ public class LoginSignupActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		// Get the view from main.xml
-		setContentView(R.layout.loginsignup);
+		setContentView(R.layout.activity_login);
 		// Locate EditTexts in main.xml
 		username = (EditText) findViewById(R.id.username);
 		password = (EditText) findViewById(R.id.password);
@@ -65,41 +65,13 @@ public class LoginSignupActivity extends Activity {
 		});
 		// Sign up Button Click Listener
 		signup.setOnClickListener(new OnClickListener() {
- 
 			public void onClick(View arg0) {
-				// Retrieve the text entered from the EditText
-				usernametxt = username.getText().toString();
-				passwordtxt = password.getText().toString();
- 
-				// Force user to fill up the form
-				if (usernametxt.equals("") && passwordtxt.equals("")) {
-					Toast.makeText(getApplicationContext(),
-							"Please complete the sign up form",
-							Toast.LENGTH_LONG).show();
- 
-				} else {
-					// Save new user data into Parse.com Data Storage
-					ParseUser user = new ParseUser();
-					user.setUsername(usernametxt);
-					user.setPassword(passwordtxt);
-					user.signUpInBackground(new SignUpCallback() {
-						public void done(ParseException e) {
-							if (e == null) {
-								// Show a simple Toast message upon successful registration
-								Toast.makeText(getApplicationContext(),
-										"Successfully Signed up, please log in.",
-										Toast.LENGTH_LONG).show();
-							} else {
-								Toast.makeText(getApplicationContext(),
-										"Sign up Error", Toast.LENGTH_LONG)
-										.show();
-							}
-						}
-					});
-				}
- 
+				Intent intent = new Intent(LoginSignupActivity.this,
+						SignUpActivity.class);
+				startActivity(intent);
 			}
 		});
+		
  
 	}
 }
